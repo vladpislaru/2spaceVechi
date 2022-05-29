10d1c7b194fc63174de5166174f853bab512fe03
Andreea-Nica <andreea.nica@eestec.ro>
2022-05-29 12:19:13 +0300

    formular gata, pagina principala retusuri plus de adaugat sustinatorii

---
+++ juries.js
@@ -0,0 +1,13 @@
+const juries = [
+    {
+      photo: "image.png",
+      name: "Daniel Rosner",
+    },
+    
+    // {
+    //   photo: "https://i.imgur.com/Hz5EhEo.jpg",
+    //   name: "Diana Ciobănete",
+    // },
+  ];
+  
+  export default juries;
